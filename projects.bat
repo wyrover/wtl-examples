c6338809dd73da1f22e56e8a60b27eefe5bb80ed
wyrover <wyrover@gmail.com>
2017-05-11 22:04:46 +0800

    + projects.bat

---
+++ projects.bat
@@ -0,0 +1,6 @@
+cd /d "%~dp0"
+mklink /d /j "E:\book-code" "H:\rover\rover-self-work\cpp\book-code" 
+set PATH=%~dp0;E:\book-code;%PATH%
+premake5 --file=projects.lua vs2013
+::premake5 --file=projects.lua vs2005
+pause
